d44bbcaaacbd6a516c0020e8c4c3b9bf2d064ee5
NITIN GAIKWAD <ngaikwda@gmail.com>
2025-01-17 12:11:58 +0530

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -12,10 +12,18 @@ end
 
 -- Header filter phase to hide response headers
 function CustomResponsePlugin:header_filter(conf)
-  -- Loop through headers to identify and mask sensitive ones
+  -- Loop through the headers to identify and remove or mask sensitive ones
   for _, header in ipairs(conf.sensitive_headers) do
-    if kong.response.get_header(header) then
-      kong.response.set_header(header, conf.replacement_text)
+    local header_value = kong.response.get_header(header)
+    if header_value then
+      kong.log.debug("Found header to mask: ", header)
+      if conf.replacement_text == "[REMOVE]" then
+        kong.log.debug("Removing header: ", header)
+        kong.response.clear_header(header) -- Remove the header completely
+      else
+        kong.log.debug("Masking header: ", header, " with value: ", conf.replacement_text)
+        kong.response.set_header(header, conf.replacement_text) -- Mask the header value
+      end
     end
   end
 end
@@ -25,3 +33,4 @@ CustomResponsePlugin.PRIORITY = 10
 CustomResponsePlugin.VERSION = "1.0.0"
 
 return CustomResponsePlugin
+
